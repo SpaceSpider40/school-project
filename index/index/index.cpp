d3a96dd6a9635a2c2192ad0a680cb209d5278979
SpaceSpider40 <60621940+SpaceSpider40@users.noreply.github.com>
2020-02-29 17:38:06 +0100

    Add files via upload

---
+++ index/index/index.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+void test(), resoults();
+string text;
+char odp[10];
+int popOdp = 0;
+
+ifstream zPliku;
+
+
+int main()
+{
+	zPliku.open("Dane.txt");
+
+	if (zPliku)
+	{
+		test();
+	}
+	else
+	{
+		cout << "===Blad===" << endl;
+		cout << "===Plik nie zostal znaleziony===";
+	}
+}
+
+void test() {
+
+	for (int i = 0;i < 10; i++)
+	{
+		cout << "=======================================" << endl;
+		cout << "===Test wiedzy z zakresu informatyki===" << endl;
+		cout << "=======================================" << endl;
+		cout << endl;
+		for (int j = 0 ;j < 6; j++)
+		{
+			getline(zPliku, text);
+			cout << text << endl;
+		}
+		cout << "Odpowiedz: ";
+		cin >> odp[i];
+		system("cls");
+	}
+	resoults();
+}
+
+void resoults() {
+
+	char txtchr;
+
+	for (int i = 0; i < 10; i++)
+	{
+		zPliku.get(txtchr);
+		if (odp[i] == txtchr) {
+			popOdp++;
+		}
+	}
+	cout << "==================" << endl;
+	cout << "=== Twoj wynik ===" << endl;
+	cout << "==================" << endl;
+	cout << endl;
+	cout << "---" << popOdp << " poprawnych odpowiedzi na 10---";
+
+}
